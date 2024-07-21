5eb4e0046401d5e7d470ef0291a61a3368607d83
Kigo-P <peterkigo33@gmail.com>
2024-07-21 13:35:46 +0300

    last Commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName){
+    let newArray = []
+
+    for(let i = 0; i < names.length; i++){ 
+    
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    
+}
+   return newArray
+}
+
+let names = ["Charlie", "Samip", "Ali"]
+// let eventName = 'birthday'
+console.log(writeCards(names,"birthday"))
+
+
+
+function countDown(num) {
+    let init = 10;
+    while(init >= 0) {
+        console.log(init--)
+    }
+    return num;
+}
